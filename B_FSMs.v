5da387d8f0c268988dda92e813298717c6eded6a
fayzanx <fayzanx@gmail.com>
2019-07-16 16:28:29 +0500

    Optimized the sequence detector so that it now uses one less flip flop. Added state table to the file for reference.

--- B_FSMs.v
+++ B_FSMs.v
@@ -5,7 +5,20 @@
 */
 
 /* [L7P1] 4-bit sequence Detector [0000 or 1111]
-    Instantiation: FSM_4xSequenceIdenticalDetect name(.fOut(), .W(), .pCLK(), .nREST()); */
+    Instantiation: FSM_4xSequenceIdenticalDetect name(.fOut(), .W(), .pCLK(), .nREST());
+    
+    + Updated for part b:
+     +---+---states----++---+------w=0----++---+------w=1----+
+    ][ A | 0 0000 0000 || B | 0 0000 0011 || F | 0 0010 0001 ][
+    ][ B | 0 0000 0011 || C | 0 0000 0101 || F | 0 0010 0001 ][
+    ][ C | 0 0000 0101 || D | 0 0000 1001 || F | 0 0010 0001 ][
+    ][ D | 0 0000 1001 || E | 0 0001 0001 || F | 0 0010 0001 ][
+    ][ E | 0 0001 0001 || E | 0 0001 0001 || F | 0 0010 0001 ][
+    ][ F | 0 0010 0001 || B | 0 0000 0011 || G | 0 0100 0001 ][
+    ][ G | 0 0100 0001 || B | 0 0000 0011 || H | 0 1000 0001 ][
+    ][ H | 0 1000 0001 || B | 0 0000 0011 || I | 1 0000 0001 ][
+    ][ I | 1 0000 0001 || B | 0 0000 0011 || I | 1 0000 0001 ][
+     +---+-------------++---+-------------++---+-------------+                */
 module FSM_4xSequenceIdenticalDetect(
     output fOut, 
     //output reg [8:0]Q,
@@ -14,11 +27,11 @@ module FSM_4xSequenceIdenticalDetect(
 	reg [8:0]Q;
     always@(posedge pCLK or negedge nREST) begin
         if(~nREST) begin
-            //RESET state = A [0 0000 0001]
-            Q[8:0] <= 9'b000000001;
+            //RESET state = A [0 0000 0000]
+            Q[8:0] <= 9'b000000000;
         end//if
         else begin
-            Q[0] <= 0;  
+            Q[0] <= 1;  
             Q[1] <= (Q[0]|Q[5]|Q[6]|Q[7]|Q[8])&(~W);
             Q[2] <= (Q[1])&(~W);
             Q[3] <= (Q[2])&(~W);
